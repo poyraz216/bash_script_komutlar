6e351750a51d1bf62742e088307a1658fefeba39
poyraz216 <poyraz216@hotmail.com>
2022-07-21 18:15:14 +0000

    crontabCheck

--- crontabCheck.sh
+++ crontabCheck.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-aktif=0
-pasif=0
+exec > /home/theadmin/bash_script_komutlar/crontabLog 2>&1
+
 input="/var/spool/cron/crontabs/theadmin"
 while IFS= read -r line
 do
-  
-  firstCharacter=${line:0:1}
-  if [ "$firstCharacter" == "#" ]
+  if [ "${#line}" -gt "35" ]
   then
-    aktif=$((aktif+1))
-    echo "$firstCharacter"
-  else
-    pasif=$((pasif+1))
-    echo "$firstCharacter"
+    firstCharacter=${line:0:1}
+    if [ "$firstCharacter" == "*" ]
+    then
+      aktif=$((aktif+1))
+      echo "$line"
+     
+    else
+      pasif=$((pasif+1))
+      #echo "$line"
+      
+    fi
   fi
-
 done < "$input"
-echo $aktif
-echo $pasif
-
